5804a08415fdb99d5a2569ae12f7b62ab2e3363f
thoak90codes <adorahtho@gmail.com>
2023-04-08 23:26:28 -0500

    added if conditions for past, present, and future. rearranged code. added function to click.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,45 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 
-// var timeBlock = $('.time-block')
+$(function (){
+  var saveBtn = $(this).find('.saveBtn')
 
-// var textArea = $('.description')
-// var pastTime = $('.past')
-// var presentTime = $('.present')
-// var futureTime = $('.future')
-
-var currentDay = $('#currentDay')
-var saveBtn = $('.saveBtn')
-
-function displayCurrentDay(){
-  var rightNow = dayjs().format('dddd, MMMM D, YYYY h:mm A')
-  currentDay.text(rightNow)
-}
-
-function setLocalStorage () {
-  $('.time-block').each(function(){
-    var textId = $(this).attr('id')
-    var textAreaValue = $(this).find('textarea').val()
-    localStorage.setItem(textId, textAreaValue)
+  saveBtn.on('click', function() {
+    $('.time-block').each(function(){
+      var textId = $(this).attr('id')
+      var textAreaValue = $(this).find('textarea').val()
+      localStorage.setItem(textId, textAreaValue)
+    })
+    console.log('save button clicked')
   })
-}
+  displayCurrentDay()
+  setInterval(displayCurrentDay, 1000)
 
-saveBtn.on('click', setLocalStorage)
+  $(document).ready(function() {
+    $('.time-block').each(function(){
 
-$(document).ready(function() {
-  $('.time-block').each(function(){
-    var textId = $(this).attr('id');
-    var savedTextAreaValue = localStorage.getItem(textId);
-    $(this).find('textarea').val(savedTextAreaValue);
-  });
-});
+      var textId = $(this).attr('id')
+      var savedTextAreaValue = localStorage.getItem(textId)
+      $(this).find('textarea').val(savedTextAreaValue)
 
+      var currentHour = dayjs().hour()
+      console.log('current hour is', currentHour)
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
+      var timeBlockHour = parseInt(textId.split('-')[1])
+      console.log('current time block', timeBlockHour)
 
+      if (timeBlockHour < currentHour) {
+        $(this).addClass('past')
+      } else if (timeBlockHour === currentHour) {
+        $(this).addClass('present')
+      } else {
+        $(this).addClass('future')
+      }
+    })
+  })
 
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  function displayCurrentDay(){
+    var currentDay = $('#currentDay')
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-
-displayCurrentDay()
-setInterval(displayCurrentDay, 1000)
+    var rightNow = dayjs().format('dddd, MMMM D, YYYY h:mm A')
+    currentDay.text(rightNow)
+  }
+})
