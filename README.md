f4ad01b1d75f2f741e4c6280dc9d3a1310014ad6
thoak90codes <adorahtho@gmail.com>
2023-04-09 14:45:18 -0500

    add comments to JavaScript file. completed readme with texs and image. minor edits to html div class.

--- README.md
+++ README.md
@@ -1,5 +1,37 @@
 # work-day-scheduler
 
+A daily planner that shows the past, current, and future time-blocks for the day.
+
+## Techonologies Used
+
+- HTML
+- CSS
+- JavaScript
+- jQuery
+- Day.js
+
+## Desciptions
+
+This daily planner shows the current day, date, and time in the header. Based on the time, the background color of each time-block will change to show which time-block was in the past, is present, and in the future. The text entered will be saved when the save button is clicked and still appear in the time-block even after the page has been refreshed.
+
+## Visuals
+
+![Daily planner showing text saved in localStorage that's color coded based on time.](./assets/images/daily-planner-ss.png)
+
+## Installation
+
+1. Clone the following repository: 
+https://github.com/coding-boot-camp/crispy-octo-meme.git
+
+2. Navigate to cloned repository to start coding.
+
 ## Deployed Page Link
 
 https://thoak90codes.github.io/work-day-scheduler/ 
+
+## Author, Contributors, & Credits
+
+- UW Coding Bootcamp
+- @thoak90codes
+- https://day.js.org/en/ 
+- https://api.jquery.com/ 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,40 @@
-
+//All code is wrapped in a call to jQuery to ensure code isn't run until the browser is done rendering all elements in the html.
 $(function (){
+
+  //Each element with the time-block class will run this function to get the value from localStorage for the divId and display it to the textarea element.
+  $('.time-block').each(function(){
+
+    var divId = $(this).attr('id')
+    var savedTextAreaValue = localStorage.getItem(divId)
+    $(this).find('textarea').val(savedTextAreaValue)
+
+    //This variable uses Day.js to get the current hour in military time.
+    var currentHour = dayjs().hour()
+    console.log('current hour is', currentHour)
+
+    //This variable grabs the divID (hour-#) and splits between the "-". "hour" and "#" will become substrings in an array of strings where the "#" will be obtained and parsed into an integer. 
+    var timeBlockHour = parseInt(divId.split('-')[1])
+    console.log('current time block', timeBlockHour)
+
+    //The if, else if, and else statements will either add the class 'past', 'present', or 'future' to the div element based on what the currentHour vs. timeBlockHour is. The background color of the time-block will change based on what the currentHour is.
+    if (timeBlockHour < currentHour) {
+      $(this).addClass('past')
+    } else if (timeBlockHour === currentHour) {
+      $(this).addClass('present')
+    } else {
+      $(this).addClass('future')
+    }
+  })
+  
+  //This function displays the current day and time using Day.js in the element with id of currentDay.
+  function displayCurrentDay(){
+    var currentDay = $('#currentDay')
+
+    var rightNow = dayjs().format('dddd, MMMM D, YYYY h:mm A')
+    currentDay.text(rightNow)
+  }
+
+  //The function will run for the saveBtn that is clicked. It will find the value entered into the textarea and setItem to localStorage. The textId will be the key and the textAreaValue will be the value.
   var saveBtn = $(this).find('.saveBtn')
 
   saveBtn.on('click', function() {
@@ -10,36 +45,7 @@ $(function (){
     })
     console.log('save button clicked')
   })
+
   displayCurrentDay()
   setInterval(displayCurrentDay, 1000)
-
-  $(document).ready(function() {
-    $('.time-block').each(function(){
-
-      var textId = $(this).attr('id')
-      var savedTextAreaValue = localStorage.getItem(textId)
-      $(this).find('textarea').val(savedTextAreaValue)
-
-      var currentHour = dayjs().hour()
-      console.log('current hour is', currentHour)
-
-      var timeBlockHour = parseInt(textId.split('-')[1])
-      console.log('current time block', timeBlockHour)
-
-      if (timeBlockHour < currentHour) {
-        $(this).addClass('past')
-      } else if (timeBlockHour === currentHour) {
-        $(this).addClass('present')
-      } else {
-        $(this).addClass('future')
-      }
-    })
-  })
-
-  function displayCurrentDay(){
-    var currentDay = $('#currentDay')
-
-    var rightNow = dayjs().format('dddd, MMMM D, YYYY h:mm A')
-    currentDay.text(rightNow)
-  }
 })
